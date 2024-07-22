93d09af84f9520e04eb3975174e9c733f5d405d6
Soumik Mukherjee <msoumik78@gmail.com>
2024-07-22 07:15:31 +0530

    cleaned main

--- bank-customers-core-data/src/main/java/org/example/BankCustomer.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import jakarta.validation.constraints.NotNull;
-
-import java.io.Serializable;
-
-public record BankCustomer(@NotNull String name, @NotNull int age, @NotNull String city,@NotNull String state, @NotNull String profession) implements Serializable {
-}
--- bank-customers-core-main/src/main/java/org/example/exception/CRMException.java
+++
@@ -1,7 +0,0 @@
-package org.example.exception;
-
-public class CRMException extends RuntimeException {
-    public CRMException(String message) {
-        super(message);
-    }
-}
