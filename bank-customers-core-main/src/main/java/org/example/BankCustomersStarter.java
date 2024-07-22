17733c67d5ca9c9d3bb948c9ae48f0320a73628d
Soumik Mukherjee <msoumik78@gmail.com>
2024-07-22 11:27:46 +0530

    fixed code

--- bank-customers-core-main/src/main/java/org/example/BankCustomersStarter.java
+++ bank-customers-core-main/src/main/java/org/example/BankCustomersStarter.java
@@ -1,11 +1,21 @@
 package org.example;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootApplication
 public class BankCustomersStarter {
     public static void main(String[] args) {
         SpringApplication.run(BankCustomersStarter.class, args);
     }
+
+  @Bean
+  public CommandLineRunner demo(JdbcTemplate jdbcTemplate) {
+    return (args) -> {
+      jdbcTemplate.update("Create table customers (customer_name VARCHAR(50),customer_age int,customer_city VARCHAR(50),customer_state VARCHAR(50),customer_profession VARCHAR(50))");
+    };
+  }
 }
--- bank-customers-core-main/src/main/java/org/example/service/BankCustomersService.java
+++ bank-customers-core-main/src/main/java/org/example/service/BankCustomersService.java
@@ -3,65 +3,41 @@ package org.example.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.BankCustomer;
-import org.example.dao.CustomerEntity;
-import org.example.dao.CustomerRepository;
-import org.example.exception.BusinessException;
-import org.springframework.cache.Cache;
-import org.springframework.cache.CacheManager;
+import org.example.dao.IDao;
+import org.example.exception.InvalidInputException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 @Transactional
 public class BankCustomersService {
+  private final IDao iDao;
 
-    private final CustomerRepository customerRepository;
+  public void createCustomer(BankCustomer bankCustomer) {
+    iDao.saveInDatabase(bankCustomer);
+  }
 
-    public void createCustomer(BankCustomer bankCustomer) {
-        customerRepository.save(convertFromModelToEntity(bankCustomer));
+  public BankCustomer getCustomerDetail(String bankCustomerName) {
+    BankCustomer bankCustomer;
+    try {
+      bankCustomer = iDao.findBankCustomerByName(bankCustomerName);
+    } catch (EmptyResultDataAccessException e) {
+      throw new InvalidInputException("Bankcustomer : "+bankCustomerName+" not found");
     }
+    return bankCustomer;
+  }
 
-    public BankCustomer getCustomerDetail(String bankCustomerName) {
-        CustomerEntity customerEntity = customerRepository.findByCustomerName(bankCustomerName);
-        if (customerEntity != null) {
-            return convertFromEntityToModel(customerEntity);
-        } else {
-            throw new BusinessException("Bankcustomer : "+bankCustomerName+" not found");
-        }
-    }
+  public void removeCustomer(String bankCustomerName) {
+    BankCustomer bankCustomer = getCustomerDetail(bankCustomerName);
+    iDao.deleteFromDatabase(bankCustomerName);
+  }
 
-    public void removeCustomer(String bankCustomerName) {
-        customerRepository.deleteByCustomerName(bankCustomerName);
-    }
-
-    public void updateCustomer(String bankCustomerName, BankCustomer bankCustomerUpdated) {
-        customerRepository.save(new CustomerEntity());
-    }
-
-    private CustomerEntity convertFromModelToEntity(BankCustomer bankCustomer) {
-        CustomerEntity customerEntity = new CustomerEntity();
-        customerEntity.setCustomerName(bankCustomer.name());
-        customerEntity.setCustomerAge(bankCustomer.age());
-        customerEntity.setCustomerCity(bankCustomer.city());
-        customerEntity.setCustomerState(bankCustomer.state());
-        customerEntity.setCustomerProfession(bankCustomer.profession());
-        return customerEntity;
-    }
-
-    private BankCustomer convertFromEntityToModel(CustomerEntity customerEntity) {
-       return new BankCustomer(
-                customerEntity.getCustomerName(),
-                customerEntity.getCustomerAge(),
-                customerEntity.getCustomerCity(),
-                customerEntity.getCustomerState(),
-                customerEntity.getCustomerProfession()
-       );
-    }
-
-
+  public void updateCustomer(String bankCustomerName, BankCustomer bankCustomerUpdated) {
+    BankCustomer bankCustomer = getCustomerDetail(bankCustomerName);
+    iDao.updateInDatabase(bankCustomerName, bankCustomerUpdated);
+  }
 
 }
